705e15f51e8d86a0eebd3ffa29b033a18e0b1fc2
Carlos Eduardo <carlos-eduardo-dev@hotmail.com>
2021-05-04 18:32:09 -0300

    feat: 1924 vitória e a indecisão

---
+++ URI/1924.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n, i;
+    scanf("%d", &n);
+    for (i = 0; i <= n; i++)
+    {
+        char buffer[101] = {0};
+        fgets(buffer, 100, stdin);
+    }
+    printf("Ciencia da Computacao\n");
+    return (0);
+}
