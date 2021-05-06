af4edd2e9215faa02e6c43b2d8febf5ff8c7bd4f
Carlos Eduardo <carlos-eduardo-dev@hotmail.com>
2021-05-05 21:19:34 -0300

    feat: 1004 produto simples

---
+++ URI/1004.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int num1, num2, PROD;
+
+    scanf("%d %d", &num1, &num2);
+
+    PROD = num1 * num2;
+
+    printf("PROD = %d\n", PROD);
+
+    return 0;
+}
